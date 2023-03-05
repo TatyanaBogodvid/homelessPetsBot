93f8f3280be911a51f17e882b6b937204e855bc7
Tatyana Bogodvid <mamchic-tatyana@mail.ru>
2023-03-05 22:20:33 +0300

    описана команда /start добавлена кнопка меню в бота

---
+++ src/main/java/com/example/homelesspetsbot/service/TelegramBot.java
@@ -0,0 +1,77 @@
+package com.example.homelesspetsbot.service;
+
+import com.example.homelesspetsbot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Приветствует пользователя"));
+        listOfCommands.add(new BotCommand("/info", "Выводит информацию о приюте"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String userName = update.getMessage().getChat().getFirstName();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, userName);
+                    break;
+                default: sendMessage(chatId, "Команда пока не поддерживается");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String userName){
+        String answer = "Привет! МЫ - ВОЛОНТЕРЫ МОСКОВСКОГО МУНИЦИПАЛЬНОГО ПРИЮТА ДЛЯ БЕЗДОМНЫХ ЖИВОТНЫХ БИРЮЛЕВО";
+        sendMessage(chatId,answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try{
+            execute(message);
+        }
+        catch (TelegramApiException e){
+
+        }
+    }
+}
