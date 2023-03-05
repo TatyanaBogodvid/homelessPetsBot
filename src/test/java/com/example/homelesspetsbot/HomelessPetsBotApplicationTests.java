c751b36e5ad19496d6ab53f94ac11554410fbd68
Tatyana Bogodvid <mamchic-tatyana@mail.ru>
2023-03-05 20:14:19 +0300

    начальный комит

---
+++ src/test/java/com/example/homelesspetsbot/HomelessPetsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homelesspetsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessPetsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
